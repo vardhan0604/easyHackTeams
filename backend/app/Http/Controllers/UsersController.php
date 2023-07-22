cd8fcd0a4a11e6af611b98f0cd3dea7b621e0af5
Prakhar Sinha <thesecretinspiration@gmail.com>
2023-07-22 16:25:09 +0530

    Feat: Added registeration logic in user controller

--- backend/app/Http/Controllers/UsersController.php
+++ backend/app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Users;
+use App\Http\Requests\storeRegisterRequest;
 
 class UsersController extends Controller
 {
@@ -20,7 +21,32 @@ class UsersController extends Controller
         $jsonResponse = json_encode(array("userExists"=>1, "userId"=>$userDetails->userid));
         return response($jsonResponse)->cookie("userId", $userDetails->userid);
     }
-    public function register(Request $request) {
+    public function register(storeRegisterRequest $request) {
+        
+        $email = $request->email;
+        $username = $request->username;
+        $userid = substr($username,0,3).uniqid();
+        $password = $request->password;
+        $interests = $request->interests;
+
+        $userExists = Users::where('email',$email)->orWhere('username', $username)->count();
+        if($userExists) {
+            return response()->json(["status"=>1, "msg"=>"User already exists!"]);
+        }
+    
+        try {
+            $newUser = new Users;
+            $newUser->email = $email;
+            $newUser->userid = $userid;
+            $newUser->username = $username;
+            $newUser->password = $password;
+            $newUser->interests = $interests;
+            $newUser->save();
+            return response()->json(["status"=>1, "msg"=>"Registeration successfull!"]);
+        }
+        catch(\Illuminate\Database\QueryException $e) {
+            return response()->json(["status"=>0, "msg"=>"Query Failed!"]);
+        }
 
     }
     public function showProfile(Request $request) {
