272183a3d99124d6460420cb7a4b675cd7b47418
Ananya Gautam <ananyagautam0607@gmail.com>
2023-07-23 11:19:07 +0530

    make homepage
    Signed-off-by: Ananya Gautam <ananyagautam0607@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,57 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar/Navbar';
-import Home from './components/home/Home';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-// import About from './About';
-// import Contact from './Contact';
+// import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+// import Login from './components/Login/Login';
+// import Register from './components/Register/Register';
+// // import About from './About';
+// // import Contact from './Contact';
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Routes>
+//
+//         {/* <Route path="/about" element={<About />} /> */}
+//         {/* <Route path="/contact" element={<Contact />} /> */}
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/register" element={<Register />} />
+//         {/* Add more routes and components for other pages */}
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import CommunityList from "./components/CommunityList";
+import Dashboard from "./components/Dashboard";
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
-        {/* <Route path="/contact" element={<Contact />} /> */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        {/* Add more routes and components for other pages */}
-      </Routes>
-    </Router>
+    <div className="App">
+      <div className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Router className="">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </div>
+      <div className="">
+        <div className="">
+          <Header />
+          <CommunityList />
+        </div>
+        <div>
+          <Dashboard />
+        </div>
+      </div>
+    </div>
   );
 };
 
