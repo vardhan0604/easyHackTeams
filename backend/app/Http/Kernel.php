e80112af9ee1f08aeaa2463c96fe1d214a06101a
vardhan0604 <harsh062004@gmail.com>
2023-07-23 13:45:07 +0530

    Revert "Fix: CORS"
    This reverts commit f6529c16fb85472908fa3424a874f8cb16320227.

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -15,7 +15,6 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
-        \App\Http\Middleware\CORS::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
@@ -54,7 +53,7 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
-        // 'cors' => App\Http\Middleware\CORS::class,
+        'cors' => 'App\Http\Middleware\CORS',
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
