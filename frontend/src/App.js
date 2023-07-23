92c694211305140c9bff7c81a75f83e0d3050b3a
Ananya Gautam <ananyagautam0607@gmail.com>
2023-07-23 12:45:33 +0530

    change app.js
    Signed-off-by: Ananya Gautam <ananyagautam0607@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,7 @@
-// import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-// import Login from './components/Login/Login';
-// import Register from './components/Register/Register';
-// // import About from './About';
-// // import Contact from './Contact';
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <Routes>
-//
-//         {/* <Route path="/about" element={<About />} /> */}
-//         {/* <Route path="/contact" element={<Contact />} /> */}
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-//         {/* Add more routes and components for other pages */}
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
+// import Home from "./components/Home";
+import { Link } from 'react-router-dom';
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
@@ -35,19 +12,24 @@ const App = () => {
   return (
     <div className="App">
       <div className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Router className="">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </Router>
+        <Navbar />
       </div>
-      <div className="">
-        <div className="">
+      <div className="d-flex">
+        <div className="left-pane col-lg-6">
+          <div id="mySidebar" className="sidebar">
+            <Link className="closebtn" onClick={() => {}}>
+              &times;
+            </Link>
+            <Link href="#">About</Link>
+            <Link href="#">Services</Link>
+            <Link href="#">Clients</Link>
+            <Link href="#">Contact</Link>
+          </div>
+
           <Header />
           <CommunityList />
         </div>
-        <div>
+        <div className="col-lg-6"> {/* Adjust the size as needed */}
           <Dashboard />
         </div>
       </div>
@@ -56,3 +38,4 @@ const App = () => {
 };
 
 export default App;
+
